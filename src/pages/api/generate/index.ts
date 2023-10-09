ffa045249f2a82105512c6c078716a670b5f7ab1
Jung Rama <jungrama.id@gmail.com>
2023-10-09 23:05:12 +0800

    fix(api): upload and ai generation fixed problem

--- src/pages/api/generate/index.ts
+++ src/pages/api/generate/index.ts
@@ -1,66 +1,55 @@
 import OpenAI from 'openai';
 import { OpenAIStream, StreamingTextResponse } from 'ai';
 
+// Create an OpenAI API client (that's edge friendly!)
 const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY ?? '',
 });
 
-import type { NextApiRequest, NextApiResponse } from 'next';
+// IMPORTANT! Set the runtime to edge: https://vercel.com/docs/functions/edge-functions/edge-runtime
+export const runtime = 'edge';
 
-type ResponseData = {
-  message: string;
-};
-
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<ResponseData>,
-) {
+export async function POST(req: Request): Promise<Response> {
   // Check if the OPENAI_API_KEY is set, if not return 400
-  // if (!process.env.OPENAI_API_KEY || process.env.OPENAI_API_KEY === '') {
-  //   res.status(400).json({
-  //     message:
-  //       'Missing OPENAI_API_KEY – make sure to add it to your .env file.',
-  //   });
-  // }
+  if (!process.env.OPENAI_API_KEY || process.env.OPENAI_API_KEY === '') {
+    return new Response(
+      'Missing OPENAI_API_KEY – make sure to add it to your .env file.',
+      {
+        status: 400,
+      },
+    );
+  }
 
-  // console.log(req.body.prompt);
+  const { prompt } = await req.json();
 
-  // let { prompt } = await req.json();
-
-  // const response = await openai.chat.completions.create({
-  //   model: 'gpt-3.5-turbo',
-  //   messages: [
-  //     {
-  //       role: 'system',
-  //       content:
-  //         'You are an AI writing assistant that continues existing text based on context from prior text. ' +
-  //         'Give more weight/priority to the later characters than the beginning ones. ' +
-  //         'Limit your response to no more than 200 characters, but make sure to construct complete sentences.',
-  //       // we're disabling markdown for now until we can figure out a way to stream markdown text with proper formatting: https://github.com/steven-tey/novel/discussions/7
-  //       // "Use Markdown formatting when appropriate.",
-  //     },
-  //     {
-  //       role: 'user',
-  //       content: req.body.prompt,
-  //     },
-  //   ],
-  //   temperature: 0.7,
-  //   top_p: 1,
-  //   frequency_penalty: 0,
-  //   presence_penalty: 0,
-  //   stream: true,
-  //   n: 1,
-  // });
-
-  res.status(200).json({ message: 'Hello from Next.js!' });
+  const response = await openai.chat.completions.create({
+    model: 'gpt-3.5-turbo',
+    messages: [
+      {
+        role: 'system',
+        content:
+          'You are an AI writing assistant that continues existing text based on context from prior text. ' +
+          'Give more weight/priority to the later characters than the beginning ones. ' +
+          'Limit your response to no more than 200 characters, but make sure to construct complete sentences.',
+        // we're disabling markdown for now until we can figure out a way to stream markdown text with proper formatting: https://github.com/steven-tey/novel/discussions/7
+        // "Use Markdown formatting when appropriate.",
+      },
+      {
+        role: 'user',
+        content: prompt,
+      },
+    ],
+    temperature: 0.7,
+    top_p: 1,
+    frequency_penalty: 0,
+    presence_penalty: 0,
+    stream: true,
+    n: 1,
+  });
 
   // Convert the response into a friendly text-stream
-  // const stream = OpenAIStream(response);
+  const stream = OpenAIStream(response);
 
   // Respond with the stream
-  // return new StreamingTextResponse(stream);
+  return new StreamingTextResponse(stream);
 }
-
-// export default async function handler(req: Request): Promise<Response> {
-
-// }
--- src/pages/api/upload/index.ts
+++ src/pages/api/upload/index.ts
@@ -1,22 +1,31 @@
 import { put } from '@vercel/blob';
-import type { NextApiRequest, NextApiResponse } from 'next';
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  const file = req.body || '';
-  const filename = req.headers['x-vercel-filename'] ?? ('file.txt' as string);
-  const contentType = req.headers['content-type'] ?? 'text/plain';
+import { NextResponse } from 'next/server';
+
+export const runtime = 'edge';
+
+export default async function POST(req: Request) {
+  if (!process.env.BLOB_READ_WRITE_TOKEN) {
+    return new Response(
+      "Missing BLOB_READ_WRITE_TOKEN. Don't forget to add that to your .env file.",
+      {
+        status: 401,
+      },
+    );
+  }
+
+  const file = req.body ?? '';
+  const filename = req.headers.get('x-vercel-filename') ?? 'file.txt';
+  const contentType = req.headers.get('content-type') ?? 'text/plain';
   const fileType = `.${contentType.split('/')[1]}`;
 
-  console.log(filename);
-  console.log('file', file);
-
   // construct final filename based on content-type if not provided
-  // const finalName = filename.includes(fileType)
-  //   ? filename
-  //   : `${filename as string}${fileType}`;
-  // const blob = await put(finalName, file, {
-  //   contentType: 'image/png',
-  //   access: 'public',
-  // });
+  const finalName = filename.includes(fileType)
+    ? filename
+    : `${filename}${fileType}`;
+  const blob = await put(finalName, file, {
+    contentType,
+    access: 'public',
+  });
 
-  // return res.json(blob);
+  return NextResponse.json(blob);
 }
